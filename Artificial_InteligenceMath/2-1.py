f253d88dd318cac468c66418bdc45188e9d6dc7e
GuByeongMo <kbmbaksa@naver.com>
2025-04-09 00:21:49 +0900

    0409 math

--- Artificial_InteligenceMath/2-1.py
+++ Artificial_InteligenceMath/2-1.py
@@ -7,6 +7,6 @@ def line_equation_from_two_points(x1, y1, x2, y2):
     eq = Eq(y - y1, m * (x - x1))  # 점-기울기 공식
     return simplify(eq)
 
-def line_equation_from_point_slope(x1, y1, m):
-    eq = Eq(y - y1, m * (x - x1))
-    return simplify(eq)
+print("2-1:", line_equation_from_two_points(1, -2, 3, 0))
+
+print("2-2:", line_equation_from_two_points(3, 5, 0, -4))
